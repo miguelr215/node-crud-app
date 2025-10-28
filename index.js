5121edec56116d773ab27dda6dd80761ba854ead
miguelr215 <drexelramos@gmail.com>
2025-10-28 10:05:35 -0400

    added api routes

--- index.js
+++ index.js
@@ -11,10 +11,44 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
+app.get("/api/products", async (req, res) => {
+	try {
+		const products = await Product.find({});
+		res.status(200).json(products);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+app.get("/api/products/:id", async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id);
+		res.status(200).json(product);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
 app.post("/api/products", async (req, res) => {
 	try {
 		const product = await Product.create(req.body);
 		res.status(200).json(product);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+app.put("/api/products/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const product = await Product.findByIdAndUpdate(id, req.body);
+
+		if (!product) {
+			return res.status(404).json({ message: "Product not found" });
+		}
+
+		const updatedProduct = await Product.findById(id);
+		res.status(200).json(updatedProduct);
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
